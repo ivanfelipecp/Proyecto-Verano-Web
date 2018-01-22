237d26dd25ccae18ff35ff6b53419c3c75a4a2a7
Fauricio <fauriciocr@gmail.com>
2018-01-22 17:44:29 -0600

    Commit
    Routes y se añadio lo de excursion

--- routes/excursiones.js
+++ routes/excursiones.js
@@ -6,3 +6,26 @@ exports.getPrincipal= function(req,res){
         res.render("e/index",{asociados:docs});
     })
 };
+// ### Transacciones de excursiones ###
+
+// + Crear
+// get
+exports.getCrear= function(req,res){
+    modelos.Asociado.find({puesto:"chofer"},function(err,choferes) {
+      modelos.Asociado.find({puesto:"guia"},function(err,guias) {
+        modelos.microbuses.find({},function(err,microbuses) {
+          res.render("excursiones/crear",{choferes:choferes},{guias:guias},{microbuses:microbuses});
+        })
+      })
+    })
+};
+
+// post
+exports.postCrear= function(req,res){
+    modelos.Excursion.create(req.body, function(err,doc){
+      if(err){
+          console.log(err);
+      }
+      res.redirect("/microbuses");
+  })
+};

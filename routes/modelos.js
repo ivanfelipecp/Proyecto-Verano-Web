52d0c5063a1bd5cbef3ad985a3195875f8a52d00
Unknown <fauriciocr@gmail.com>
2018-01-22 16:02:31 -0600

    Commit
    Cambios en el server.js y creacion de las rutas

---
+++ routes/modelos.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/proyectoDB');
+
+exports.Asociado = mongoose.model("asociado",
+    {
+        nombre: String,
+        apellido: String,
+        email: String,
+        telefono: Number,
+        puesto: String
+    }
+);
+
+exports.Microbus = mongoose.model("Microbus",
+    {
+        propietario:{"type":mongoose.Schema.Types.ObjectId,ref:"asociado"},
+        estadoMecanico: String,
+        servicios: Array
+    }
+);
+
+exports.Excursion = mongoose.model("excursion",
+    {
+        guia:{"type":mongoose.Schema.Types.ObjectId,ref:"asociado"},
+        chofer:{"type":mongoose.Schema.Types.ObjectId,ref:"asociado"},
+        cantMaxPersonas: Number,
+        microbuses: Array,
+        fechaHoraSalida: Date,
+  	    lugaresDeAbordaje: Array,
+  	    fechaHoraVuelta: Date,
+        costos: Array,
+        comidas: Array
+    }
+);

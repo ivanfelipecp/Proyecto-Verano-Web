2af85185e1dcc69516cad1e12e6bd862ea2901fa
Anthony <Anthonylle@hotmai.com>
2018-01-24 00:24:14 -0600

    Vv

--- routes/clientes/excursiones.js
+++ routes/clientes/excursiones.js
@@ -1,9 +1,11 @@
 var modelos = require("../modelos");
-
+var utils = require("../utils");
 // ### Transacciones de excursiones para los CLIENTES ###
 // Get principal
 exports.getPrincipal= function(req,res){
+    console.log("LA PUTA MADRE IVAN");
     modelos.Excursion.find({}).populate("guia").populate("destino").populate("microbuses.microbus").exec(function(err,docs){
+
         console.log(docs);
         res.render("clientes/excursiones/index",{excursiones:docs});
     });
